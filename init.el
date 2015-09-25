fbe8c01195ca62330381e2ad85a1200eca6fbef7
sobataro <sobatarooo@gmail.com>
2015-09-25 12:44:57 +0900

    Initial commit

---
+++ init.el
@@ -0,0 +1,52 @@
+;; load-path を追加する関数
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory
+              (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; 各ディレクトリとそのサブディレクトリを load-path に追加
+(add-to-load-path "elisp" "conf" "public_repos")
+
+
+
+;; 文字コードの設定
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; Mac の場合 (ファッキン unicode 正規化)
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+
+
+;; カラーテーマ
+(when (require 'color-theme nil t)
+  (color-theme-initialize)
+  (color-theme-hober))
+
+;; モードラインにカラム番号を表示
+(column-number-mode t)
+
+;; 行番号を表示
+(global-linum-mode t)
+(setq linum-format "%d ")
+
+;; カーソル行に下線
+(setq hl-line-face 'underline)
+(global-hl-line-mode)
+
+;; インデントはスペースのみ
+(setq-default indent-tabs-mode nil)
+
+;; 対応する括弧のハイライト
+(setq show-paren-delay 0)
+(show-paren-mode t)
+
+;; "C-c l" で line wrap を切り替え
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
